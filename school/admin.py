7f8ef823a158d963a48a3a6ecb6c2fcfae5084f3
SteveIsReal <pyn.zabulata@gmail.com>
2025-05-27 13:18:35 +0700

    model update

--- school/admin.py
+++ school/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Student, Teacher
 
-# Register your models here.
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'age']
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ['name','email','phone']
